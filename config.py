58be4795e9f048d89c48b6b0ec287a7b63fb5cd0
Hassan Juma <okothhassanjuma@gmail.com>
2020-10-18 15:53:11 +0300

    add a class

--- config.py
+++ config.py
@@ -1,15 +1,18 @@
 import os
+
+
 class Config:
     '''
     General configuration parent class
     '''
-    NEWS_API_HEADLINES_URL='https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
-    NEWS_API_SOURCES_URL ='https://newsapi.org/v2/sources?apiKey={}'
-    NEWS_API_HEADLINES_BY_SOURCE_URL ='http://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
-    NEWS_API_HEADLINES_BY_SEARCH_URL='http://newsapi.org/v2/top-headlines?q={}&sortBy=publishedAt&apiKey={}'
-    NEWS_API_HEADLINES_BY_CATEGORY_URL ='http://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}'
+    NEWS_API_HEADLINES_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
+    NEWS_API_SOURCES_URL = 'https://newsapi.org/v2/sources?apiKey={}'
+    NEWS_API_HEADLINES_BY_SOURCE_URL = 'http://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    NEWS_API_HEADLINES_BY_SEARCH_URL = 'http://newsapi.org/v2/top-headlines?q={}&sortBy=publishedAt&apiKey={}'
+    NEWS_API_HEADLINES_BY_CATEGORY_URL = 'http://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}'
 
-    NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+
 
 class ProdConfig(Config):
     '''
@@ -31,7 +34,8 @@ class DevConfig(Config):
 
     DEBUG = True
 
+
 config_options = {
-'development':DevConfig,
-'production':ProdConfig
+    'development': DevConfig,
+    'production': ProdConfig
 }
